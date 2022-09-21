d75d7f7d6929c81d5a6d794012ded36234deba5d
Boos <christianboos@rocketmortgage.com>
2022-09-20 20:25:09 -0400

    coffee

---
+++ Lab11_2_CoffeShop/Controllers/HomeController.cs
@@ -0,0 +1,48 @@
+﻿using Lab11_2_CoffeShop.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace Lab11_2_CoffeShop.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        public IActionResult RegisterHandler(string fName, string lName, string email)
+        {
+            
+           string outPut = $"Congrats {fName} {lName}, on your registration! We'll send you a confirmation at {email}!";
+            ViewBag.outPut = outPut;
+            return View();
+        }
+
+
+
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Lab11_2_CoffeShop/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Lab11_2_CoffeShop.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
